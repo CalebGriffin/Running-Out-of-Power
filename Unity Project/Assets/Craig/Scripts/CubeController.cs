d365f0451a9be9db118df6585b5667a4b35af46b
craigzeki <craig.zeki@gmail.com>
2022-09-25 23:35:43 +0100

    Player movement, 2D physics, cube rotation

---
+++ Unity Project/Assets/Craig/Scripts/CubeController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private static CubeController instance;
+
+    public enum CubeStates
+    {
+        STATIC = 0,
+        ROTATING,
+        NUM_OF_STATES
+    }
+
+    [SerializeField] private float rotationAngle = 90.0f;
+    [SerializeField] private float rotationLerpDuration = 3.0f;
+    [SerializeField] private List<Vector3> rotationSequence;
+
+    private int rotationSequenceIndex = 0;
+    private Vector3 eulerLerpOutput = Vector3.zero;
+    private Vector3 eulerLerpStart = Vector3.zero;
+    private Vector3 eulerLerpEnd = Vector3.zero;
+
+    private Quaternion qStart = Quaternion.Euler(Vector3.zero);
+    private Quaternion qEnd = Quaternion.Euler(Vector3.zero);
+    private Vector3 positionStart = Vector3.zero;
+    private Vector3 positionEnd = Vector3.zero;
+    private float rotationRate = 0.0f;
+
+    private CubeStates cubeState = CubeStates.STATIC;
+    public CubeStates CubeState { get => cubeState; }
+    public static CubeController Instance
+    {
+        get
+        {
+            if (instance == null) instance = FindObjectOfType<CubeController>();
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (rotationSequence.Count <= 0)
+        {
+            Debug.LogError("Error: rotationSequence not set!");
+            return;
+        }
+        rotationRate = rotationAngle / rotationLerpDuration;
+
+        
+
+        eulerLerpStart = eulerLerpOutput = gameObject.transform.eulerAngles = rotationSequence[rotationSequenceIndex];
+        qStart = transform.rotation;
+        qEnd = transform.rotation;
+        positionEnd = transform.position;
+        positionStart = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(cubeState)
+        {
+            case CubeStates.STATIC:
+
+                transform.rotation = qEnd;
+                transform.position = positionEnd;
+                break;
+            case CubeStates.ROTATING:
+                
+
+                break;
+            
+            case CubeStates.NUM_OF_STATES:
+            default:
+                // should not get here
+                break;
+        }
+        //transform.eulerAngles = eulerLerpOutput;
+    }
+
+    IEnumerator rotateFunc()
+    {
+        for(float i = 0.0f; i < rotationAngle; i += Time.deltaTime * rotationRate)
+        {
+            yield return null;
+            transform.Rotate(rotationSequence[rotationSequenceIndex], Time.deltaTime * rotationRate);
+        }
+        cubeState = CubeStates.STATIC;
+    }
+
+    public void rotateNext()
+    {
+        if (cubeState == CubeStates.ROTATING) return;
+        
+        if(++rotationSequenceIndex >= rotationSequence.Count) return;
+
+        qStart = transform.rotation;
+        positionStart = transform.position;
+
+        transform.Rotate(rotationSequence[rotationSequenceIndex], rotationAngle);
+
+        qEnd = transform.rotation;
+        positionEnd = transform.position;
+
+        transform.rotation = qStart;
+        transform.position = positionStart;
+
+        StartCoroutine(rotateFunc());
+
+        cubeState = CubeStates.ROTATING;
+
+        //transform.RotateAround()
+        //rotationSequenceIndex++;
+        //eulerLerpStart = Vector3.Scale(transform.eulerAngles, rotationSequence[rotationSequenceIndex]);
+        //eulerLerpEnd = Vector3.Scale()
+        //StartCoroutine(LerpRotation());
+        //cubeState = CubeStates.ROTATING;
+    }
+
+
+
+    //IEnumerator LerpRotation()
+    //{
+    //    float timeElapsed = 0;
+
+    //    while (timeElapsed < rotationLerpDuration)
+    //    {
+    //        eulerLerpOutput = Vector3.Lerp(eulerLerpStart, rotationSequence[rotationSequenceIndex], timeElapsed / rotationLerpDuration);
+    //        timeElapsed += Time.deltaTime;
+    //        yield return null;
+    //    }
+
+    //    eulerLerpOutput = rotationSequence[rotationSequenceIndex];
+    //    cubeState = CubeStates.STATIC;
+    //}
+}
---
+++ Unity Project/Assets/Craig/Scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float jumpForce = 1.0f;
+    [SerializeField] private float playerGravity = -0.1f;
+
+
+    [SerializeField] private float stickDeadbandThreshold = 0.3f;
+
+    private bool leftPressed = false;
+    private bool rightPressed = false;
+    private bool southPressed = false;
+
+    private Rigidbody2D myRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(leftPressed)
+        {
+            gameObject.transform.position += (Vector3.left * speed * Time.deltaTime);
+        }
+        if (rightPressed)
+        {
+            gameObject.transform.position += (Vector3.right * speed * Time.deltaTime);
+        }
+        if (southPressed)
+        {
+            myRB.AddForce(Vector2.up * jumpForce);
+            southPressed = false;
+        }
+
+        gameObject.transform.position -= (Vector3.down * playerGravity);
+    }
+
+    private bool checkOutsideDeadband(float axis, float deadbandThreshold)
+    {
+        return (Mathf.Abs(axis) >= deadbandThreshold ? true : false);
+    }
+
+    private void OnLeft_Stick(InputValue value)
+    {
+        // Get the Vector2 from the input value
+        Vector2 inputVector = value.Get<Vector2>();
+        Debug.Log("Left Stick: " + inputVector.ToString());
+        if(checkOutsideDeadband(inputVector.x, stickDeadbandThreshold))
+        {
+            if(inputVector.x < 0)
+            {
+                leftPressed = true;
+            }
+            else
+            {
+                rightPressed = true;
+            }
+        }
+        else
+        {
+            leftPressed = false;
+            rightPressed = false;
+        }
+
+    }
+
+    // Gives an argument for a Vector2 for the direction in which the joystick is being pressed
+    private void OnRight_Stick(InputValue value)
+    {
+        // Get the Vector2 from the input value
+        Vector2 inputVector = value.Get<Vector2>();
+        Debug.Log("Right Stick: " + inputVector.ToString());
+        // Do something with the input here
+    }
+
+    private void OnSouth_Button()
+    {
+        // This will run when the south button is pressed
+        Debug.Log("South Button Pressed");
+        southPressed = true;
+    }
+
+    private void OnEast_Button()
+    {
+        // This will run when the east button is pressed
+        CubeController.Instance.rotateNext();
+    }
+}
