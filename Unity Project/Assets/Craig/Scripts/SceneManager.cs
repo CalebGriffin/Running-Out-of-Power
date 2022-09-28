8e08fc9c42b7c9dc3f16aafa7aceaf3d8075e99e
craigzeki <craig.zeki@gmail.com>
2022-09-28 14:22:05 +0100

    Removed SceneSwitcher - prevented compilation - now fixed

--- Unity Project/Assets/Craig/Scripts/SceneManager.cs
+++ Unity Project/Assets/Craig/Scripts/SceneManager.cs
@@ -1,18 +1,58 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour
+public static class SceneManager
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private static List<LevelInfo> levels;
+
+    public static int currentLevel = 0;
+
+
+    public static void AddLevel(string name, string sceneName, string menuDisplayInfo = "")
     {
-        
+        levels.Add(new LevelInfo(name, sceneName, menuDisplayInfo));
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void LoadLevel(int level)
     {
-        
+        currentLevel = level;
+
+    }
+
+
+    public static void ReturnToMenu()
+    {
+
+    }
+
+    public static void ReturnToMenu(string displayText)
+    {
+
+    }
+
+}
+
+public class LevelInfo
+{
+    public string name;
+    public string menuDisplayInfo;
+    public string sceneName;
+
+    public LevelInfo(string name, string sceneName)
+    {
+        this.name = name;
+        this.sceneName = sceneName;
+        this.menuDisplayInfo = "";
+    }
+
+    public LevelInfo(string name, string sceneName, string menuDisplayInfo)
+    {
+        this.name = name;
+        this.sceneName = sceneName;
+        this.menuDisplayInfo = menuDisplayInfo;
+
     }
 }
