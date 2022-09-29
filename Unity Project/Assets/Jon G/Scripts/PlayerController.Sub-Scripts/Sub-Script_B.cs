9efcf4042a1288fa60686b66130b33ab9192513a
AGuyNamedJon <75310638+AGuyNamedJon@users.noreply.github.com>
2022-09-29 21:02:40 +0100

    E

--- Unity Project/Assets/Jon G/Scripts/PlayerController.Sub-Scripts/Sub-Script_B.cs
+++ Unity Project/Assets/Jon G/Scripts/PlayerController.Sub-Scripts/Sub-Script_B.cs
@@ -5,20 +5,30 @@ public partial class PlayerController
 {
     private void Action_Run()
     {
-        Debug.Log("Running");
+        if (powerLevel < runCost) { return; }
+
+        running = !(running);
     }
     private void Run_Extension()
     {
-        
+        if (powerLevel < runCost) { running = false; return; }
+
+        if (!(running)) { return; }
+
+        RigidBody().AddForce((new Vector2(MoveSpeed() * Direction(), 0f) * RigidBody().mass) - RigidBody().velocity, ForceMode2D.Force);
     }
 
     private void Action_Jump()
     {
-        Debug.Log("Jumping");
+        if (powerLevel < jumpCost) { return; }
+        
+        RigidBody().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
     }
 
     private void Action_Flip() 
     {
-        Debug.Log("Flipping");
+        if (powerLevel < flipCost) { return; }
+
+        flipped = !(flipped);
     }
 }
--- Unity Project/Assets/Jon G/Scripts/PlayerController.cs
+++ Unity Project/Assets/Jon G/Scripts/PlayerController.cs
@@ -5,22 +5,35 @@ using UnityEngine.UI;
 
 public partial class PlayerController : MonoBehaviour
 {
-    // Player Input Reference
-        private PlayerInput Player()
-        { return GetComponent<PlayerInput>(); }
 
-    // Input Data
+    /* Player Input Reference */
+        private PlayerInput Player() { return GetComponent<PlayerInput>(); }
+
+    /* Player Component References */
+        private Rigidbody2D RigidBody() { return GetComponent<Rigidbody2D>(); }
+
+    /* Enumerations*/
+        [System.Serializable] private enum CurrentAction { Run, Jump, Flip }
+
+    /* Variables */ [Header("Variables")]
+        [SerializeField] private float moveSpeed = 450f;
+        [SerializeField] private float jumpForce = 15f;
+
+    /* Attributes */
+        private float MoveSpeed() { return Mathf.Abs(moveSpeed) * Time.fixedDeltaTime; }
+        private float Direction() { return !(flipped) ? 1f : -1f; }
+
+    /* Input Data */
         private bool canGoLeft = false, canGoRight = false;
         private Coroutine leftRoutine, rightRoutine;
 
-    // States
-        [SerializeField] private uint powerLevel = 0;
+    /* States */
+        private bool running = false;
+        private bool flipped = false;
+        private uint powerLevel = 0;
+        private CurrentAction currentAction;
 
-        [System.Serializable]
-        private enum CurrentAction { Run, Jump, Flip }
-        [SerializeField] private CurrentAction currentAction;
-
-    // Action Level Requirements
+    /* Action Level Requirements */
         private uint runCost = 0;
         private uint jumpCost = 3;
         private uint flipCost = 1;
@@ -32,10 +45,7 @@ public partial class PlayerController : MonoBehaviour
     
     private void OnLeft_Stick(InputValue value)
     {
-        // Get the Vector2 from the input value
         Vector2 inputVector = value.Get<Vector2>();
-
-        // Do something with the input here
 
         if (inputVector.x >= 0.5f)
         { 
@@ -65,10 +75,6 @@ public partial class PlayerController : MonoBehaviour
 
     private void OnSouth_Button()
     {
-        // This will run when the south button is pressed
-
-        Debug.Log($"Executing {currentAction.ToString()} Action");
-
         Invoke("Action_" + currentAction.ToString(), 0f);
     }
 }
